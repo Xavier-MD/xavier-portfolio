75af2cea04f03ae74986d22d4252a469e30834fd
Xavier-MD <xavier.pmd@outlook.com>
2024-02-04 16:51:01 -0500

    Added boilerplate for project MVP.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import ProjectsPage from './pages/projects_page';
+
+function Model({ url }) {
+  const gltf = useLoader(GLTFLoader, url);
+  return <primitive object={gltf.scene} />;
+}
+
 function App() {
   return (
     <div className='App'>
-      <h1>React App</h1>
+      <div className='Viewport w-screen h-screen overflow-hidden'>
+        <ProjectsPage />
+      </div>
     </div>
   );
 }
