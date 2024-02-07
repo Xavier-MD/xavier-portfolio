22fbad6b93b2cf8609118a6910cdb7708134e384
Xavier-MD <xavier.pmd@outlook.com>
2024-02-07 14:10:52 -0500

    Added custom colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        xWhite: '#F8F4F9',
+        xWhite2: '#E5EBEA',
+        xBlack: '#1A181B',
+        xPurple: '#827191',
+        xOrange: '#FFD6AF',
+        xRed: '#AD2E24',
+        xGreen: '#08605F',
+        xBlue: '#B4D4EE',
+        xPink: '#B098A4',
+        xGrey: '#747572',
+        xPlatinum: '#DBD9DB'
       }
     }
   },
