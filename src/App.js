e468b60bc5b18e740608c58d756c8aba3d1f25c5
Xavier-MD <xavier.pmd@outlook.com>
2024-02-07 06:58:03 -0500

    Moved viewport sizing from index.css to App.js.

--- src/App.js
+++ src/App.js
@@ -14,31 +14,79 @@ import getUuid from 'uuid-by-string';
 
 const GOLDENRATIO = 1.61803398875;
 
-export const App = ({ images }) => (
-  <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
-    <color attach='background' args={['#1A181B']} />
-    <fog attach='fog' args={['#1A181B', 0, 10]} />
-    <group position={[0, -0.8, 0]}>
-      <Frames images={images} />
-      <mesh rotation={[-Math.PI / 2, 0, 0]}>
-        <planeGeometry args={[50, 50]} />
-        <MeshReflectorMaterial
-          blur={[800, 800]}
-          resolution={2048}
-          mixBlur={1}
-          mixStrength={20}
-          roughness={1}
-          depthScale={1.2}
-          minDepthThreshold={0.5}
-          maxDepthThreshold={1.4}
-          color='#050505'
-          metalness={0.7}
-        />
-      </mesh>
-    </group>
-    <Environment preset='city' />
-  </Canvas>
-);
+const pexel = (id) =>
+  `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
+const images = [
+  // Front
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
+  // Back
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+  // Left
+  {
+    position: [-1.75, 0, 0.25],
+    rotation: [0, Math.PI / 2.5, 0],
+    url: pexel(327482)
+  },
+  {
+    position: [-2.15, 0, 1.5],
+    rotation: [0, Math.PI / 2.5, 0],
+    url: pexel(325185)
+  },
+  {
+    position: [-2, 0, 2.75],
+    rotation: [0, Math.PI / 2.5, 0],
+    url: pexel(358574)
+  },
+  // Right
+  {
+    position: [1.75, 0, 0.25],
+    rotation: [0, -Math.PI / 2.5, 0],
+    url: pexel(227675)
+  },
+  {
+    position: [2.15, 0, 1.5],
+    rotation: [0, -Math.PI / 2.5, 0],
+    url: pexel(911738)
+  },
+  {
+    position: [2, 0, 2.75],
+    rotation: [0, -Math.PI / 2.5, 0],
+    url: pexel(1738986)
+  }
+];
+
+export default function App() {
+  return (
+    <div className='App'>
+      <div className='Viewport w-screen h-screen overflow-hidden'>
+        <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
+          <color attach='background' args={['#1A181B']} />
+          <fog attach='fog' args={['#1A181B', 0, 10]} />
+          <group position={[0, -0.8, 0]}>
+            <Frames images={images} />
+            <mesh rotation={[-Math.PI / 2, 0, 0]}>
+              <planeGeometry args={[50, 50]} />
+              <MeshReflectorMaterial
+                blur={[800, 800]}
+                resolution={2048}
+                mixBlur={1}
+                mixStrength={20}
+                roughness={1}
+                depthScale={1.2}
+                minDepthThreshold={0.5}
+                maxDepthThreshold={1.4}
+                color='#050505'
+                metalness={0.7}
+              />
+            </mesh>
+          </group>
+          <Environment preset='city' />
+        </Canvas>
+      </div>
+    </div>
+  );
+}
 
 function Frames({
   images,
