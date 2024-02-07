34d9095029c82d0486e81eae422904aa8fee50bc
Xavier-MD <xavier.pmd@outlook.com>
2024-02-07 14:11:36 -0500

    Added overlay component.

---
+++ src/components/overlay.js
@@ -0,0 +1,16 @@
+export default function Overlay() {
+  return (
+    <div className='Overlay'>
+      <div className='absolute top-10 right-44 font-sans text-base text-end'>
+        Professional
+        <br />
+        Portfolio
+      </div>
+      <div className='absolute top-10 right-10'>
+        —
+        <br />
+        02/23
+      </div>
+    </div>
+  );
+}
